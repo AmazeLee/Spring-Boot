184b72cc9e204f373cc60c55706f4cfdc256694e
AmazeLee <911711054@qq.com>
2017-11-25 10:30:01 +0800

    spring boot各类配置

--- src/main/java/com/sh/springboot/demo/HelloApplication.java
+++ src/main/java/com/sh/springboot/demo/HelloApplication.java
@@ -2,10 +2,14 @@ package com.sh.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.nio.charset.Charset;
 
 @Controller
 @SpringBootApplication
@@ -16,6 +20,13 @@ public class HelloApplication {
     @ResponseBody
     public String hello(){
         return "hello world！";
+    }
+
+    //自定义消息转化器，spring boot 默认是utf-8
+    @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter(){
+        StringHttpMessageConverter converter  = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return converter;
     }
     
     public static void main(String[] args) {
